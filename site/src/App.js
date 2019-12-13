142c99a06fa5241362a96ba86b049a04b9959407
RagnnnaCode <filigreepvp@mail.ru>
2019-12-13 21:28:30 +0300

    Creating

--- site/src/App.js
+++ site/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FirstPage from './Components/FirstPage/FirstPage';
+
 
 function App() {
   return (
     <div className="App">
-     
+        <FirstPage/>
     </div>
   );
 }
--- site/src/Components/FirstPage/FirstPage.jsx
+++ site/src/Components/FirstPage/FirstPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import style from './FP.module.css'
 
 const FirstPage = (props) => {
     return(
-        <div></div>
+        <div>
+            <div className={style.all}>
+            <div className={style.wrapper}>
+
+            </div>
+            </div>
+        </div>
     )
 }
 export default FirstPage;
