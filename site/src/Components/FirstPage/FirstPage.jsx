84e170afa004b1183d0219a14f088fa6e457685f
RagnnnaCode <filigreepvp@mail.ru>
2019-12-14 17:50:33 +0300

    A low update service page

--- site/src/Components/FirstPage/FirstPage.jsx
+++ site/src/Components/FirstPage/FirstPage.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import style from './FP.module.css'
 import {Link} from 'react-router-dom';
 import WOW from 'wowjs';
--- site/src/Components/ServicesPage/Services.jsx
+++ site/src/Components/ServicesPage/Services.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import style from './Services.module.css'
+import {Link} from 'react-router-dom';
+import WOW from 'wowjs';
 
 const Services = (props) => {
     return(
-        <div>
-            Services
+        <div className={style.wrapperS}>
+            <div className={style.NavBar}>
+                <ul className="list1" >
+                    <li ><Link to="/">Home</Link></li>
+                    <li className={style.listcomp2}><Link to="/Services">Services</Link></li>
+                    <li><Link to="/About">About</Link></li>
+                    <li><Link to="/Work">Work</Link></li>
+                    <li><Link to="/Contacts">Contacts</Link></li>
+                </ul>
+            </div>
+            <div className={style.acrostia}>
+                <p>acrostia</p>
+            </div>
         </div>
     )
 }
