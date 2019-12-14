93a9aa661b0510e99405e150e98f41d7d700dc2f
RagnnnaCode <filigreepvp@mail.ru>
2019-12-14 14:55:17 +0300

    Update with WOW.js

--- site/src/Components/FirstPage/FirstPage.jsx
+++ site/src/Components/FirstPage/FirstPage.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import style from './FP.module.css'
 import {Link} from 'react-router-dom';
+import WOW from 'wowjs';
 
-const FirstPage = (props) => {
+const FirstPage = () => {
+    
+    const m = "wow fadeInUp";
+        new WOW.WOW().init();
+    
     return(
         <div className={style.wrapper}>
             <div className={style.all}>
@@ -15,8 +20,20 @@ const FirstPage = (props) => {
                         <li><Link to="/Contacts">Contacts</Link></li>
                     </ul>
                 </div>
+                <div className={m} data-wow-delay="0.25s" data-wow-duration="2.5s">
+                    <div className={style.preview}  >
+                        <div className={style.previewFirst}>
+                            <p>good ideas</p>
+                            <p>comes</p>
+                        </div>
+                        <div className={style.previewSecond}>
+                            <p>first!</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
+
 export default FirstPage;
